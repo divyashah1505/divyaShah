7bfa01bf1c37bfadaaba1657c3d5fe21f21924bf
divyashah1505 <divya.1505shah@gmail.com>
2026-04-29 11:21:16 +0530

    Added root health check routes

--- src/App.js
+++ src/App.js
@@ -309,6 +309,7 @@ app.post("/stripe/webhook", express.raw({ type: 'application/json' }), async (re
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
+app.use("/uploads", express.static(path.join(__dirname, "../uploads/IMG")));
 app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 app.use("/api/users", router);
 app.use("/api/admin", adminRouter);
